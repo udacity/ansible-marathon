a3232b75cba8ce8f988dae127067ecd4bf85ce8e
James Earl Douglas <james@earldouglas.com>
2016-05-12 10:30:24 -0600

    Move module to ansible_marathon package

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ setup(
     version='0.1.0',
     description=' An Ansible module for deploying applications to Mesos through Marathon',
     url='https://github.com/udacity/ansible-marathon',
+    py_modules=[
+        'ansible_marathon.marathon',
+    ],
     author='James Earl Douglas',
     author_email='james@earldouglas.com',
 )
