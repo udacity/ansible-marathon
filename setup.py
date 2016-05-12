7954a2d26d90f770dbb52f191a1e9d45e468be1f
James Earl Douglas <james@earldouglas.com>
2016-05-12 10:18:02 -0600

    Add setup.py for pypi publishing

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='ansible-marathon',
+    version='0.1.0',
+    description=' An Ansible module for deploying applications to Mesos through Marathon',
+    url='https://github.com/udacity/ansible-marathon',
+    author='James Earl Douglas',
+    author_email='james@earldouglas.com',
+)
